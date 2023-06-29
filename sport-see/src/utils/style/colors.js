70d1a70ec25a4444fbb84810b9ea10f203bdadd0
Marine <marine.magnin73@gmail.com>
2023-06-29 21:36:17 +0200

    CSS : ajout du fichier couleurs

---
+++ sport-see/src/utils/style/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  primary: '#FF0101',
+  secondary: '#000000',
+  tertiary: '#FFFFFF',
+  colorInfos: '#74798C',
+  colorProtein: '#4AB8FF', // Blue
+  colorGlucid: '#FDCC0C', // Yellow
+  colorLipid: '#FD5181', // Pink
+  backgroundDark: '#020203',
+  backgroundLight: '#FBFBFB',
+}
+
+export default colors
