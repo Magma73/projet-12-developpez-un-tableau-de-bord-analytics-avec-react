62b82cbde041bcae58aff30b7c1bf78435626c1e
Marine <marine.magnin73@gmail.com>
2023-07-12 18:30:33 +0200

    Suppression code inutile

--- sport-see/src/index.jsx
+++ sport-see/src/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Error from './pages/Error';
 import Home from './pages/Home';
 import Header from './components/Header';
 import SlideNav from './components/SlideNav';
-// import styles from './utils/style/'
-
-// import '.utils/style/style.css';
-import './style.css';
-
 import styled from 'styled-components'
-// import colors from '../../utils/style/colors'
+import './style.css';
 
 const MainContainer = styled.main`
     display:flex;
@@ -26,18 +20,11 @@ root.render(
       <MainContainer>
         <SlideNav />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
           <Route exact path="/" element={<Navigate to="/user/12" />} />
           <Route path="/user/:userId" element={<Home />} />
-          {/* <Route path="/reglages" element={<Settings />} /> */}
-          {/* <Route path="/communaute" element={<Community />} /> */}
           <Route path="*" element={<Error />} />
         </Routes>
       </MainContainer>
     </Router>
   </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
