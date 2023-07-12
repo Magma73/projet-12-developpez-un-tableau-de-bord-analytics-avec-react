af8c082967e38277b9e891d44b880e179f901e54
Marine <marine.magnin73@gmail.com>
2023-07-12 18:01:20 +0200

    Js : ajout du model PerformanceData

--- sport-see/src/models/Performance.js
+++ sport-see/src/models/Performance.js
@@ -16,7 +16,6 @@ export default class Performance {
       5: 'Vitesse',
       6: 'Intensité',
     }
-    // this._kind = data.kind
     this._data = data.data.map((element) => new PerformanceData(element))
   }
 
@@ -26,7 +25,6 @@ export default class Performance {
    */
   get kind() {
     return this._kindMapping
-    // return Object.keys(this._kind).map((kindNumber) => this._kindMapping[this._kind[kindNumber]])
   }
 
   /**
@@ -37,28 +35,3 @@ export default class Performance {
     return this._data
   }
 }
-
-// export default class Performance {
-//   constructor(data) {
-//     this._kind = {
-//       1: 'cardio',
-//       2: 'energy',
-//       3: 'endurance',
-//       4: 'strength',
-//       5: 'speed',
-//       6: 'intensity',
-//     }
-//     this._data = data.map((element) => ({
-//       value: element.value,
-//       kind: this._kind[element.kind],
-//     }))
-//   }
-
-//   get kind() {
-//     return this._kind
-//   }
-
-//   get data() {
-//     return this._data
-//   }
-// }
